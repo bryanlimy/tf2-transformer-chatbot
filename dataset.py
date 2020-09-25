15ae95cdb13f971b7a85e9c2b29dddb29f37c801
Bryan Li <bryanlimy@gmail.com>
2020-09-25 15:41:24 +0800

    update

--- dataset.py
+++ dataset.py
@@ -10,10 +10,28 @@ def preprocess_sentence(sentence):
   # eg: "he is a boy." => "he is a boy ."
   sentence = re.sub(r"([?.!,])", r" \1 ", sentence)
   sentence = re.sub(r'[" "]+', " ", sentence)
+  # removing contractions
+  sentence = re.sub(r"i'm", "i am", sentence)
+  sentence = re.sub(r"he's", "he is", sentence)
+  sentence = re.sub(r"she's", "she is", sentence)
+  sentence = re.sub(r"it's", "it is", sentence)
+  sentence = re.sub(r"that's", "that is", sentence)
+  sentence = re.sub(r"what's", "that is", sentence)
+  sentence = re.sub(r"where's", "where is", sentence)
+  sentence = re.sub(r"how's", "how is", sentence)
+  sentence = re.sub(r"\'ll", " will", sentence)
+  sentence = re.sub(r"\'ve", " have", sentence)
+  sentence = re.sub(r"\'re", " are", sentence)
+  sentence = re.sub(r"\'d", " would", sentence)
+  sentence = re.sub(r"\'re", " are", sentence)
+  sentence = re.sub(r"won't", "will not", sentence)
+  sentence = re.sub(r"can't", "cannot", sentence)
+  sentence = re.sub(r"n't", " not", sentence)
+  sentence = re.sub(r"n'", "ng", sentence)
+  sentence = re.sub(r"'bout", "about", sentence)
   # replacing everything with space except (a-z, A-Z, ".", "?", "!", ",")
   sentence = re.sub(r"[^a-zA-Z?.!,]+", " ", sentence)
   sentence = sentence.strip()
-  # adding a start and an end token to the sentence
   return sentence
 
 
